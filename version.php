5e4bb3cc4f711b91b256d7b821c9484f00395cc3
rezeau <joseph@rezeau.org>
2019-01-13 18:37:29 +0100

    Bumped version number for new release 3.6.1
    Signed-off-by: rezeau <joseph@rezeau.org>

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_regexpadaptivewithhelpnopenalty';
-$plugin->version  = 2018050801;
-$plugin->requires = 2015051100; // Moodle version.
+$plugin->version  = 2019011301;
+$plugin->requires = 2016052300; // Moodle version.
 $plugin->dependencies = array(
-    'qbehaviour_regexpadaptivewithhelp' => 2018050801
+    'qbehaviour_regexpadaptivewithhelp' => 2019011301
 );
-$plugin->release = '3.5.1 for Moodle 2.9-3.5';
+$plugin->release = '3.6.1 for Moodle 3.1-3.6';
 $plugin->maturity  = MATURITY_STABLE;
