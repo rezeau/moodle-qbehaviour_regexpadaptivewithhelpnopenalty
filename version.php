046b8d916b353a2e0d35dae0ca5116a671377d00
rezeau <joseph@rezeau.org>
2019-05-29 15:36:32 +0200

    updated version to moodle 3.7

--- version.php
+++ version.php
@@ -25,10 +25,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_regexpadaptivewithhelpnopenalty';
-$plugin->version  = 2019011301;
-$plugin->requires = 2016052300; // Moodle version.
+$plugin->version  = 2019052100;
+$plugin->requires = 2019052000; // Moodle version.
 $plugin->dependencies = array(
-    'qbehaviour_regexpadaptivewithhelp' => 2019011301
+    'qbehaviour_regexpadaptivewithhelp' => 2019052100
 );
-$plugin->release = '3.6.1 for Moodle 3.1-3.6';
+$plugin->release = '3.7.0 for Moodle 3.1-3.7';
 $plugin->maturity  = MATURITY_STABLE;
